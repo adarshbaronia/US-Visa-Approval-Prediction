d9b5baad028763bae484165e002f67311eb5a1ca
Adarsh Raj Baronia <adarshbaronia@MacBook-Air.local>
2025-04-02 16:09:14 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 # Python file for project
+from setuptools import setup, find_packages
+setup(
+    name='us_visa',
+    version='0.0.0',
+    packages=find_packages(),
+    author="Adarsh Raj Baronia",
+    author_email="a.a4273@gmail.com"
+)
--- test.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import numpy as np
-s = 'son'
-print(f"{s}")
